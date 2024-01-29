2324c042b7394bb80245c27c09b7ffd00aa4f4a7
César Gómez <cesargomez015@gmail.com>
2024-01-29 22:30:26 +0100

    version 1.1

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,3 +1,4 @@
+import {useRef, useEffect, useState, useCallback} from 'react';
 import logo from "../assets/svg/logo.svg";
 import logo2 from "../assets/logos/Logo2-removebg 2.png";
 import three_lines from "../assets/svg/3-lines.svg";
@@ -5,36 +6,59 @@ import logo_sidebar from "../assets/svg/logo-opened-sidebar.svg";
 import hide_button from "../assets/svg/hide-button.svg";
 import heart from "../assets/svg/heart.svg";
 
-const sideBar = () => {
-  const header = document.querySelector('header');
-  const three_lines_element = document.querySelector('#three_lines');
-  const logo1_element = document.querySelector('#logo');
-  const logo2_element = document.querySelector('#logo2');
-  header.classList.toggle('bg-[#019F6C]');
-  // Alternar los SVGs
-  logo2_element.classList.toggle('mix-blend-difference');
-  logo1_element.setAttribute('src', logo1_element.getAttribute('src') === logo_sidebar ? logo : logo_sidebar);
-  three_lines_element.setAttribute('src', three_lines_element.getAttribute('src') === hide_button ? three_lines : hide_button);
-  // Alternar el menú
-  const side_bar = document.querySelector('#side_bar');
-  side_bar.classList.toggle('hidden');
-  // section.classList.toggle('fixed');
-};
-
-
 const MobileHeader = () => {
+  const headerRef = useRef(null);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    if (isSidebarOpen) {
+      return;
+    }
+
+    if (window.scrollY > 0) {
+      headerRef.current.classList.remove('opacity-100');
+      headerRef.current.classList.add('opacity-60');
+    } else {
+      headerRef.current.classList.remove('opacity-60');
+      headerRef.current.classList.add('opacity-100');
+    }
+  }, [isSidebarOpen]);
+
+  const sideBar = useCallback(() => {
+    setSidebarOpen(!isSidebarOpen);
+    const header = headerRef.current;
+    header.classList.toggle('bg-[#019F6C]');
+    header.classList.toggle('bg-white');
+    if (window.scrollY !== 0) {
+      header.classList.toggle('opacity-60');
+    }
+    const logo1_element = document.querySelector('#logo');
+    const logo2_element = document.querySelector('#logo2');
+    const three_lines_element = document.querySelector('#three_lines');
+    logo2_element.classList.toggle('mix-blend-plus-lighter');
+    logo1_element.setAttribute('src', logo1_element.getAttribute('src') === logo_sidebar ? logo : logo_sidebar);
+    three_lines_element.setAttribute('src', three_lines_element.getAttribute('src') === hide_button ? three_lines : hide_button);
+  }, [isSidebarOpen]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
     <>
-      <header className="bg-white flex justify-between items-center p-5">
+      <header ref={headerRef} className="bg-white sticky top-0 flex justify-between items-center p-5 opacity-100 transition-opacity duration-75">
         <img src={logo} id="logo" alt="logo" className="h-10" />
         <img src={logo2} id="logo2" alt="logo2" className="h-7" />
         <button type="button" onClick={sideBar}>
           <img src={three_lines} id="three_lines" alt="three_lines" className="h-9 w-9" />
         </button>
       </header>
-      <section id="side_bar" className="hidden h-dvh bg-[#019F6C]">
-        <nav className="flex-col absolute top-[15%] left-[15%] space-y-10 text-3xl text-[#B9E2B6]">
-          <ul className="space-y-12">
+      <section id="side_bar" className={`${isSidebarOpen ? 'fixed w-full h-full' : 'hidden'} bg-[#019F6C] z-10`}>
+        <nav className="flex-col absolute top-[10%] left-[15%]">
+          <ul className="space-y-14 text-3xl text-[#B9E2B6]">
             <li>HISTORIA</li>
             <li>QUIENES SOMOS</li>
             <li>GALERÍA</li>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,12 @@
 import MobileHeader from "../components/MobileHeader";
-import imagenGrupo2 from "../assets/Imágenes/Fotos presentación home/imagenGrupo2.jpg";
+import imagenGrupo2 from "../assets/Imgs/Fotos presentación home/imagenGrupo2.jpg";
+import imagen2 from "../assets/Imgs/Fotos presentación home/imagen2.jpg";
+import imagenGrupo from "../assets/Imgs/Fotos presentación home/imagenGrupo.jpg";
+import icon1 from "../assets/icons/icon1.png";
+import icon2 from "../assets/icons/icon2.png";
+import icon3 from "../assets/icons/icon3.png";
+import icon4 from "../assets/icons/icon4.png";
+import icon5 from "../assets/icons/icon5.png";
 
 const Home = () => {
   return (
@@ -14,8 +21,36 @@ const Home = () => {
             SIN IRSE MEJOR Y MÁS FELIZ&quot;
           </h3>
         </section>
-        <img src={imagenGrupo2} alt="imagenGrupo2" className="h-full" />
-        <h3 className="text-center pt-5">ACCIÓN FORMACIÓN Y AVENTURA</h3>
+        <section>
+          <img src={imagen2} alt="imagen2" />
+          <img src={imagenGrupo} alt="imagenGrupo" />
+          <img src={imagenGrupo2} alt="imagenGrupo2" />
+        </section>
+        <h3 className="text-center text-xl p-4 text-[#49A87C]">ACCIÓN, FORMACIÓN Y AVENTURA</h3>
+        <section>
+          <ul className="text-[#49A87C] flex justify-around  text-sm">
+            <li className="grid place-items-center">
+              <p>Voluntariados</p>
+              <img src={icon1} alt="icon1" className="w-8" />
+            </li>
+            <li className="grid place-items-center">
+              <p>Conferencias</p>
+              <img src={icon2} alt="icon2" className="w-8" />
+            </li>
+            <li className="grid place-items-center">
+              <p>Convivencias</p>
+              <img src={icon3} alt="icon3" className="w-8" />
+            </li>
+            <li className="grid place-items-center">
+              <p>Excursiones</p>
+              <img src={icon4} alt="icon4" className="w-8" />
+            </li>
+            <li className="grid place-items-center">
+              <p>Actividades</p>
+              <img src={icon5} alt="icon5" className="w-8" />
+            </li>
+          </ul>
+        </section>
       </main>
     </>
   );
